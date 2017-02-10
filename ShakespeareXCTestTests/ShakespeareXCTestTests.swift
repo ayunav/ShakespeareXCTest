cb40460e2edf81133133a2b001ad1eaaa634897f
Ayuna NYC <ayunav@users.noreply.github.com>
2017-02-10 00:11:49 -0500

    Add performance test

--- ShakespeareXCTestTests/ShakespeareXCTestTests.swift
+++ ShakespeareXCTestTests/ShakespeareXCTestTests.swift
@@ -35,16 +35,18 @@ class ShakespeareXCTestTests: XCTestCase {
         XCTAssertEqual(playData.wordCounts["swearing"], 10, "swearing does not appear 10 times")
     }
     
+    
+    
 //    func testExample() {
 //        // This is an example of a functional test case.
 //        // Use XCTAssert and related functions to verify your tests produce the correct results.
 //    }
-//    
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            _ = PlayData()
+        }
+    }
     
 }
