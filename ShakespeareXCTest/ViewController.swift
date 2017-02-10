0c5685199a13941e898574d8a673a9d2791be36f
Ayuna NYC <ayunav@users.noreply.github.com>
2017-02-09 23:43:54 -0500

    Add tableView methods

--- ShakespeareXCTest/ViewController.swift
+++ ShakespeareXCTest/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var playData = PlayData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return playData.allWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let word = playData.allWords[indexPath.row]
+        cell.textLabel?.text = word
+        
+        return cell
+    }
+    
+    
 }
 
