1aab3dff3e1aa90c98d52d6f3296ecef1ce13368
Ayuna NYC <ayunav@users.noreply.github.com>
2017-02-09 23:43:25 -0500

    Add PlayData model class

---
+++ ShakespeareXCTest/PlayData.swift
@@ -0,0 +1,36 @@
+//
+//  PlayData.swift
+//  ShakespeareXCTest
+//
+//  Created by Ayuna NYC on 2/9/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import Foundation
+
+class PlayData {
+    
+    var allWords = [String]()
+    
+    var wordCounts = [String: Int]()
+    
+    init() {
+        if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
+            if let plays = try? String(contentsOfFile: path) {
+                allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
+                allWords = allWords.filter { $0 != "" }
+                
+                for word in allWords {
+                    if wordCounts[word] == nil {
+                        wordCounts[word] = 1
+                    } else {
+                        wordCounts[word]! += 1
+                    }
+                }
+                
+                allWords = Array(wordCounts.keys)
+            }
+        }
+    }
+    
+}
